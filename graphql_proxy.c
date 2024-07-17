e1e0acda4705c175e0ffaa76cf345beb2f86e267
tatyanakrivonogova <tatyanakrivonogova@github.com>
2024-07-17 14:15:19 +0700

    Fixed all warnings

--- graphql_proxy.c
+++ graphql_proxy.c
@@ -1,6 +1,5 @@
 #include "postgres.h"
 
-#include "coro.h"
 #include "http_parser.h"
 #include "fmgr.h"
 #include "utils/builtins.h"
@@ -41,10 +40,6 @@ typedef struct conn_info {
 
 static bool serv_logs = true;
 static ExecutorStart_hook_type prev_ExecutorStart = NULL;
-static struct ev_loop *loop;
-static struct coro_context main_context;
-static struct coro_context server_context;
-static struct coro_context client_context;
 conn_info conns[MAX_CONNECTIONS];
 char bufs[MAX_CONNECTIONS][MAX_MESSAGE_LEN];
 
@@ -118,6 +113,15 @@ graphql_proxy_start_worker(void) {
 void
 graphql_proxy_main(Datum main_arg) {
     struct sockaddr_in client_addr;
+    struct io_uring_params params;
+    struct io_uring ring;
+    int cqe_count;
+    const int val = 1;
+    struct sockaddr_in sockaddr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(DEFAULT_PORT),
+        .sin_addr.s_addr = INADDR_ANY,
+    };
     socklen_t client_len = sizeof(client_addr);
 
     int listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -127,13 +131,6 @@ graphql_proxy_main(Datum main_arg) {
         return;
     }
 
-    struct sockaddr_in sockaddr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(DEFAULT_PORT),
-        .sin_addr.s_addr = INADDR_ANY,
-    };
-
-    const int val = 1;
     setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
     if (bind(listen_socket, (struct sockaddr *)&sockaddr, sizeof(sockaddr))) {
@@ -148,8 +145,6 @@ graphql_proxy_main(Datum main_arg) {
         goto socket_listen_fail;
     }
 
-    struct io_uring_params params;
-    struct io_uring ring;
     memset(&params, 0, sizeof(params));
 
     if (io_uring_queue_init_params(MAX_ENTRIES, &ring, &params)) {
@@ -157,9 +152,9 @@ graphql_proxy_main(Datum main_arg) {
         ereport(ERROR, errmsg("uring_init_params() error: %s\n", errorbuf));
     }
 
-    // if (!(params.features & IORING_FEAT_FAST_POLL)) {
-    //     elog(LOG, "IORING_FEAT_FAST_POLL is not available in the kernel((\n");
-    // }
+    if (!(params.features & IORING_FEAT_FAST_POLL)) {
+        elog(LOG, "IORING_FEAT_FAST_POLL is not available in the kernel((\n");
+    }
     
     add_accept(&ring, listen_socket, (struct sockaddr *) &client_addr, &client_len);
 
@@ -167,19 +162,21 @@ graphql_proxy_main(Datum main_arg) {
     {
         struct io_uring_cqe *cqe;
         int ret;
+        struct io_uring_cqe *cqes[DEFAULT_BACKLOG_SIZE];
 
         io_uring_submit(&ring);
 
         ret = io_uring_wait_cqe(&ring, &cqe);
         assert(ret == 0);
 
-        struct io_uring_cqe *cqes[DEFAULT_BACKLOG_SIZE];
-        int cqe_count = io_uring_peek_batch_cqe(&ring, cqes, sizeof(cqes) / sizeof(cqes[0]));
+        cqe_count = io_uring_peek_batch_cqe(&ring, cqes, sizeof(cqes) / sizeof(cqes[0]));
 
         for (int i = 0; i < cqe_count; i++) {
+            struct conn_info *user_data;
+            unsigned type;
             cqe = cqes[i];
-            struct conn_info *user_data = (struct conn_info *) io_uring_cqe_get_data(cqe);
-            unsigned type = user_data->type;
+            user_data = (struct conn_info *) io_uring_cqe_get_data(cqe);
+            type = user_data->type;
 
             if (type == ACCEPT) {
                 int sock_conn_fd = cqe->res;
@@ -192,7 +189,7 @@ graphql_proxy_main(Datum main_arg) {
                 } else {
                     //parse input
                     int outputSize;
-                    parse_input(&bufs[user_data->fd], bytes_read, &outputSize, user_data->fd);
+                    parse_input((char*)&bufs[user_data->fd], bytes_read, &outputSize, user_data->fd);
                     add_socket_write(&ring, user_data->fd, outputSize);
                 }
             } else if (type == WRITE) {
@@ -203,10 +200,8 @@ graphql_proxy_main(Datum main_arg) {
 
     }
 
-accept_io_handle_alloc_fail:
 socket_listen_fail:
 socket_bind_fail:
-socket_set_nonblock_fail:
     close(listen_socket);
     return;
 }
@@ -223,13 +218,12 @@ parse_input(char* request, size_t request_len, int* outputSize, int fd) {
 
     char* parsed_method;
     char* parsed_path;
-    char* parsed_header_name;
-    char* parsed_header_value;
+    char* parsed_header_name = NULL;
+    char* parsed_header_value = NULL;
 
     char* query_begin;
-    char *query;
+    char *query = NULL;
     size_t query_len;
-    ssize_t res;
     int err;
     *outputSize = 0;
     elog(LOG, "read from client: %ld\n", request_len);
@@ -238,14 +232,16 @@ parse_input(char* request, size_t request_len, int* outputSize, int fd) {
     if (err == -1 || err == -2) {
         printf("send_request_to_server(): failed while parse HTTP request, error %d\n", err);
         *outputSize = sizeof("Failed while parse HTTP resuest. Change and try again\n");
-        strcpy(&bufs[fd], "Failed while parse HTTP request. Change and try again\n");
+        strcpy((char*)&bufs[fd], "Failed while parse HTTP request. Change and try again\n");
         // res = write(io_handle->fd, "Failed while parse HTTP request. Change and try again\n", 
         //             sizeof("Failed while parse HTTP resuest. Change and try again\n"));
         goto close_client_connection;
     }
 
     parsed_method = (char*)malloc(method_len + 1);
+    if (!parsed_method) goto parsed_method_malloc_fail;
     parsed_path = (char*)malloc(path_len + 1);
+    if (!parsed_path) goto parsed_path_malloc_fail;
     parsed_method[method_len] = '\0';
     parsed_path[path_len] = '\0';
 
@@ -260,7 +256,9 @@ parse_input(char* request, size_t request_len, int* outputSize, int fd) {
     // parsing of headers
     for (size_t i = 0; i < num_headers; ++i) {
         parsed_header_name = (char*)malloc(headers[i].name_len + 1);
+        if (!parsed_header_name) goto parsed_header_name_malloc_fail;
         parsed_header_value = (char*)malloc(headers[i].value_len + 1);
+        if (!parsed_header_value) goto parsed_header_value_malloc_fail;
         parsed_header_name[headers[i].name_len] = '\0';
         parsed_header_value[headers[i].value_len] = '\0';
         strncpy(parsed_header_name, headers[i].name, headers[i].name_len);
@@ -275,6 +273,7 @@ parse_input(char* request, size_t request_len, int* outputSize, int fd) {
     } else {
         query_len = request + request_len - query_begin - 2;
         query = (char*)malloc(query_len+1);
+        if (!query) goto query_malloc_fail;
         query[query_len] = '\0';
         strncpy(query, query_begin + 2, query_len);
         elog(LOG, "query_len: %ld query: %s\n", query_len, query);
@@ -282,25 +281,32 @@ parse_input(char* request, size_t request_len, int* outputSize, int fd) {
         *outputSize = query_len;
     }
     //res = write(io_handle->fd, query, (size_t)query_len);
-    elog(LOG, "buffer after query pars: %s", &bufs[fd]);
+    elog(LOG, "buffer after query pars: %s", (char*)&bufs[fd]);
 
     // close connection after completing request
-close_client_connection:
-    if (parsed_method) free(parsed_method);
-    if (parsed_path) free(parsed_path);
-    if (parsed_header_name) free(parsed_header_name);
-    if (parsed_header_value) free(parsed_header_value);
+
     if (query) free(query);
+query_malloc_fail:
+    if (parsed_header_value) free(parsed_header_value);
+parsed_header_value_malloc_fail:
+    if (parsed_header_name) free(parsed_header_name);
+parsed_header_name_malloc_fail:
+    free(parsed_path);
+parsed_path_malloc_fail:
+    free(parsed_method);
+parsed_method_malloc_fail:
+close_client_connection:
     elog(LOG, "memory is free\n");
     return;
 }
 
 void
 add_accept(struct io_uring *ring, int fd, struct sockaddr *client_addr, socklen_t *client_len) {
+    conn_info *conn_i;
     struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
     io_uring_prep_accept(sqe, fd, client_addr, client_len, 0);
 
-    conn_info *conn_i = &conns[fd];
+    conn_i = &conns[fd];
     conn_i->fd = fd;
     conn_i->type = ACCEPT;
 
@@ -309,12 +315,15 @@ add_accept(struct io_uring *ring, int fd, struct sockaddr *client_addr, socklen_
 
 void
 add_socket_read(struct io_uring *ring, int fd, size_t size) {
-    elog(LOG, "Start socket_write");
-    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-    io_uring_prep_recv(sqe, fd, &bufs[fd], size, 0);
-    elog(LOG, "Read buf from fd = %d: %s, size: %d", fd, &bufs[fd], size);
+    struct io_uring_sqe *sqe;
+    conn_info *conn_i;
 
-    conn_info *conn_i = &conns[fd];
+    elog(LOG, "Start socket_write");
+    sqe = io_uring_get_sqe(ring);
+    io_uring_prep_recv(sqe, fd, &bufs[fd], size, 0);
+    elog(LOG, "Read buf from fd = %d: %s, size: %ld", fd, (char*)&bufs[fd], size);
+
+    conn_i = &conns[fd];
     conn_i->fd = fd;
     conn_i->type = READ;
 
@@ -323,13 +332,16 @@ add_socket_read(struct io_uring *ring, int fd, size_t size) {
 
 void
 add_socket_write(struct io_uring *ring, int fd, size_t size) {
+    conn_info *conn_i;
+    struct io_uring_sqe *sqe;
+
     elog(LOG, "Start socket_write");
-    elog(LOG, "Write buf into fd = %d: %s, size: %d", fd, &bufs[fd], size);
-    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    elog(LOG, "Write buf into fd = %d: %s, size: %ld", fd, (char*)&bufs[fd], size);
+    sqe = io_uring_get_sqe(ring);
     elog(LOG, "Get uring sqe done");
     io_uring_prep_send(sqe, fd, &bufs[fd], size, 0);
 
-    conn_info *conn_i = &conns[fd];
+    conn_i = &conns[fd];
     conn_i->fd = fd;
     conn_i->type = WRITE;
 
