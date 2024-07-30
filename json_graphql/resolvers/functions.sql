22f36a167335288955b0384485ab3d2c93185167
tatyanakrivonogova <tatyanakrivonogova@github.com>
2024-07-30 10:32:44 +0700

    Added tests for query and update mutation

--- json_graphql/resolvers/functions.sql
+++ json_graphql/resolvers/functions.sql
@@ -1,3 +1,5 @@
 getPerson='SELECT json_agg("sub/1") AS Person FROM graphql_proxy.Person, LATERAL (SELECT person.id, person.name) AS "sub/1" WHERE ((person.id) = %d);';
 
 createPerson='INSERT INTO graphql_proxy.Person(id, name) VALUES(%d, %s);';
+
+updatePerson='UPDATE graphql_proxy.Person SET name = %s WHERE id = %d;';
--- test.c
+++ test.c
@@ -107,28 +107,37 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nquery {\n  getPerson(id: 8)\n}", 
-    //           "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{ \"data\": { \"person\": [{\"id\":8,\"name\":\"Ben\"}, \n {\"id\":8,\"name\":\"Ben\"}, \n {\"id\":8,\"name\":\"Tom\"}, \n {\"id\":8,\"name\":\"Tom\"}, \n {\"id\":8,\"name\":\"Tom\"}] } }");
+    // Query person by id. Return ok 200 with empty response
+    check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nquery {\n  getPerson(id: 123) {\n    id\n    name\n  }\n}",
+              "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{ \"data\": { \"person\":  } }");
 
-    // Simple mutation
+    // Insert mutation. Return ok 200 with null-data
     check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nmutation {\n  createPerson(id: 123, name: \"Tom\") {\n    id\n    name\n  }\n}",
               "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{ \"data\": { (null) } }");
 
-    // Simple mutation with default id value
+    // Insert mutation with default id value. Return ok 200 with null-data
     check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nmutation {\n  createPerson(name: \"Tom\")\n}",
               "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{ \"data\": { (null) } }");
 
-    // Unspecified value for non-null argument
+    // Insert mutation with unspecified value for non-null argument. Return error 400
     check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nmutation {\n  createPerson(id: 123) {\n    id\n    name\n  }\n}",
               "HTTP/1.1 400 Bad Request\nContent-Type: text/plain\n\nFailed while parse HTTP resuest. Change and try again\n");
 
-    // Two mutations in one operation
+    // Two insert mutations in one operation. Return error 400
     check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nmutation {\n  createPerson(name: \"Tom\") {\n    id\n    name\n  }\n  createPerson(name: \"Tom\") {\n    id\n    name\n  }\n}",
               "HTTP/1.1 400 Bad Request\nContent-Type: text/plain\n\nFailed while parse HTTP resuest. Change and try again\n");
 
-    // Two operations in one request
+    // Two insert operations in one request. Return error 400
     check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nmutation {\n  createPerson(name: \"Tom\") {\n    id\n    name\n  }\n}\nmutation {\n  createPerson(name: \"Tom\") {\n    id\n    name\n  }\n}", 
               "HTTP/1.1 400 Bad Request\nContent-Type: text/plain\n\nFailed while parse HTTP resuest. Change and try again\n");
+
+    // Update mutation. Return ok 200 with null-data
+    check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nmutation {\n  updatePerson(id: 123, name: \"Tim\") {\n    id\n    name\n  }\n}",
+              "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{ \"data\": { (null) } }");
+
+    // Query person by id. Return ok 200 with inserted and updated row
+    check(sd, "POST /query HTTP/1.1\nAccept-Encoding: gzip, deflate, br, zstd\nAccept-Language: ru,en;q=0.9\nConnection: keep-alive\nContent-Length: 104\nHost: localhost:8080\nOrigin: http://localhost:8080\nReferer: http://localhost:8080/\nAccept: application/json, multipart/mixed\nContent-type: application/json\n\nquery {\n  getPerson(id: 123) {\n    id\n    name\n  }\n}",
+              "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{ \"data\": { \"person\": [{\"id\":123,\"name\":\"Tim\"}] } }");
 
     printf("All tests was executed. OK: %ld FAIL: %ld\n", ok, fail);
     return 0;
